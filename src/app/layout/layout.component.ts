eddae1ac366be8332b7f300f09cf03aefa3c9b66
vivekGithub43 <vivekcherry143@gmail.com>
2025-01-08 20:50:54 +0530

    latest

--- src/app/layout/layout.component.ts
+++ src/app/layout/layout.component.ts
@@ -1,12 +1,13 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { CustomService } from '../custom.service';
 import { FormsModule } from '@angular/forms';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-layout',
   standalone: true,
-  imports: [FormsModule,RouterOutlet],
+  imports: [FormsModule,RouterOutlet,RouterLink,CommonModule],
   templateUrl: './layout.component.html',
   styleUrl: './layout.component.css'
 })
@@ -19,4 +20,11 @@ this.products=res.data;
 console.log(res.data);
   }); 
 }  
+headerList:any=[
+  {id:1,name:'Get the app'},
+  {id:2,name:'Investor Relation'},
+  {id:3,name:'Add Resturent'},
+  {id:4,name:'Login'},
+  {id:5,name:'SignUp'}
+]
 }
